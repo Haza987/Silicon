8e964888a6168abc18ef23d8c06ef638004fd4bf
Harry Holmes <harryholmesse@gmail.com>
2024-10-04 16:07:49 +0200

    FINAL VERSION

--- darkmode.js
+++ darkmode.js
@@ -23,9 +23,31 @@ darkmodeSwitch.addEventListener('change', () => {
   if(darkmodeSwitch.checked) {
     enableDarkMode()
     localStorage.setItem('darkmode', 'on')
+
+  // I used ChatGPT for help on how to switch images between dark mode
+  // "document.querySelectorAll('.app').forEach(anchor" looks through the HTML for all anchor tags with the class of "app"
+  // this is the same for "document.querySelectorAll('.app-dark').forEach(anchor" but instead looks for anchor tags with the class of "app-dark"
+  // "anchor.style.display =" allows me to set the desired style depending on whether the website is in dark mode or light mode.
+
+  document.querySelectorAll('.app').forEach(anchor => {
+    anchor.style.display = 'none';
+  });
+
+  document.querySelectorAll('.app-dark').forEach(anchor => {
+    anchor.style.display = 'flex';
+  });
+
   } else {
     disableDarkMode()
     localStorage.setItem('darkmode', 'off')
+
+    document.querySelectorAll('.app').forEach(anchor => {
+      anchor.style.display = 'flex';
+  });
+
+  document.querySelectorAll('.app-dark').forEach(anchor => {
+      anchor.style.display = 'none';
+  });
   }
 })
 
